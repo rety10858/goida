db6768e0a4876003d82da0b61b338020782ad9d2
rety10858 <rety10858@gmail.com>
2024-10-17 15:06:23 +0000

    22222

---
+++ 3.py
@@ -0,0 +1,8 @@
+a = int(input())
+b = int(input())
+c = int(input())
+for i in range(1,c):
+    a=a*b
+print(a)
+
+
---
+++ 5.py
@@ -0,0 +1,7 @@
+a = int(input())
+b = int(input())
+if b == 0:
+    print('Так нельзя')
+else: 
+    print(a%b)
+    print(a//b)
